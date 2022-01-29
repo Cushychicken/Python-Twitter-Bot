090eb5007a48f1d605ecb8232a77b07312541769
Nash Reilly <nrc.reilly@gmail.com>
2022-01-29 12:31:08 -0500

    Added example credentials file

---
+++ example_credentials.py
@@ -0,0 +1,14 @@
+# This is just a sample config file for Twitter app credentials.
+# You need to add these from your Twitter app settings.
+
+# Twitter calls this the API Key
+consumer_key    = 'example'
+# Twitter calls this the API Key - Secret
+consumer_secret = 'example'
+bearer_token    = 'example'
+
+# Note: to allow yourself write access for RT/Likes, you must 
+# change your OAuth settings. You will need to generate a new 
+# access token and access token secret when you do this. 
+access_token    = 'example'
+access_token_secret = 'example'
