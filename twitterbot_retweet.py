8113d634e9b7cdce47364b1d7f68b24ad8e27be4
Nash Reilly <nrc.reilly@gmail.com>
2022-01-29 15:25:37 -0500

    Rewrote twitterbot using v2 API bindings

--- twitterbot_retweet.py
+++ twitterbot_retweet.py
@@ -2,6 +2,8 @@ import tweepy
 from time import sleep
 from credentials import *
 from config import QUERY, FOLLOW, LIKE, SLEEP_TIME
+from datetime import datetime
+
 
 client = tweepy.Client(
     consumer_key=consumer_key, consumer_secret=consumer_secret,
@@ -16,35 +18,40 @@ print("Like Tweets :", LIKE)
 print("Follow users :", FOLLOW)
 print()
 
-response = client.search_recent_tweets(
-        QUERY, 
-        max_results=20,
-        expansions=["attachments.media_keys", "author_id"]
-)
+while True:
+    expansions=["attachments.media_keys", "author_id"]
+    response = client.search_recent_tweets(
+            QUERY, 
+            max_results=20,
+            expansions=expansions
+    )
 
-# In this case, the data field of the Response returned is a list of Tweet
-# objects
-tweets = response.data
+    # Returns list of Tweet objects
+    tweets = response.data
 
-# You can then access those objects in the includes Response field
-includes = response.includes
-users = includes["users"]
+    includes = response.includes
+    users = includes["users"]
 
-# An efficient way of matching expanded objects to each data object is to
-# create a dictionary of each type of expanded object, with IDs as keys
-users = {user["id"]: user for user in users}
-for tweet in tweets:
-    if users[tweet.author_id].username != 'RTLjobs':
-        client.retweet(tweet.id)
-        print("Retweeted " + str(tweet.id) + " by " + users[tweet.author_id].username)
-        if LIKE:
-            client.like(tweet.id)
+    # creates dict of each type of expanded object, with IDs as keys
+    users = {user["id"]: user for user in users}
+    for tweet in tweets:
+        if users[tweet.author_id].username != 'RTLjobs':
+            client.retweet(tweet.id)
+            print("Retweeted " + str(tweet.id) + " by " + users[tweet.author_id].username)
+            if LIKE:
+                client.like(tweet.id)
 
-        if FOLLOW :
-            try:
-                client.follow(tweet.author_id)
-            except:
-                pass
+            if FOLLOW:
+                try:
+                    client.follow_user(tweet.author_id)
+                except:
+                    pass
 
-        sleep(1)
+            sleep(1) # Politeness interval
+
+    # Sleep until next retweet iteration
+    now = datetime.now()
+    current_time = now.strftime("%I:%M:%S %p")
+    print("Check complete at " + current_time + "; next run in 15 mins...")
+    sleep(SLEEP_TIME)
 
