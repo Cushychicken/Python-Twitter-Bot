7c0d8b0f68f34aa19612127b59030b88fbbdcacf
Nash Reilly <nrc.reilly@gmail.com>
2022-02-02 08:01:32 -0500

    Added more detailed instructions to 'example_credentials.py'

--- example_credentials.py
+++ example_credentials.py
@@ -1,14 +1,23 @@
 # This is just a sample config file for Twitter app credentials.
 # You need to add these from your Twitter app settings.
+# 
+# It is STRONGLY recommended that you add credentials in this order:
+#   * rename this file to 'credentials.py'
+#   * add 'credentials.py' to .gitignore
+#   * modify 'credentials.py' to include your Twitter secret info
 
 # Twitter calls this the API Key
 consumer_key    = 'example'
 # Twitter calls this the API Key - Secret
 consumer_secret = 'example'
+# Twitter calls this the Bearer Token
 bearer_token    = 'example'
 
 # Note: to allow yourself write access for RT/Likes, you must 
-# change your OAuth settings. You will need to generate a new 
-# access token and access token secret when you do this. 
+# change your OAuth settings by enabling OAuth 1.0 and enabling read/
+# write permissions. 
+
+# It is advised to generate access_token/access_token_secret after you 
+# change OAuth permissions. Otherwise, you will need to regenerate.
 access_token    = 'example'
 access_token_secret = 'example'
